e013bab0a698fd3dbba10d7bebbaf24bccf11b63
Frank Geimer <frank.geimer@telekom.de>
2020-01-21 12:58:47 +0100

    Corses and wizard modyfied

--- src/main/java/com/wildcodeschool/wildandwizard/repository/WizardRepository.java
+++ src/main/java/com/wildcodeschool/wildandwizard/repository/WizardRepository.java
@@ -1,8 +1,9 @@
 package com.wildcodeschool.wildandwizard.repository;
 
-import com.wildcodeschool.wildandwizard.entity.Wizard;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
+
+import com.wildcodeschool.wildandwizard.entity.Wizard;
 
 @Repository
 public interface WizardRepository extends JpaRepository<Wizard, Long> {
